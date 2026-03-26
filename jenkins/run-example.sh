ad645468c8d5e51b9ef877066b93598d6d51d4f5
dyrnq <dyrnq@qq.com>
2026-03-26 11:10:47 +0800

    Update run-example.sh

--- jenkins/run-example.sh
+++ jenkins/run-example.sh
@@ -1,18 +1,50 @@
 #!/usr/bin/env bash
 
+proxy=${proxy:-}
+noproxy=${noproxy:-}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --proxy)
+            proxy="$2"
+            shift
+            ;;
+        --no-proxy|--noproxy)
+            noproxy="$2"
+            shift
+            ;;
+        --*)
+            echo "Illegal option $1"
+            ;;
+    esac
+    shift $(( $# > 0 ? 1 : 0 ))
+done
+
+# ip4=$(/sbin/ip -o -4 addr list "${iface}" | awk '{print $4}' |cut -d/ -f1 | head -n1);
+
+curl_opts_array=()
+if [ -n "${proxy}" ]; then
+  curl_opts_array+=("--proxy $proxy")
+fi
+if [ -n "${noproxy}" ]; then
+  curl_opts_array+=("--noproxy $noproxy")
+fi
+curl_opts="${curl_opts_array[*]}"
+echo "curl_opts=${curl_opts}"
+
 name="jenkins"
 host_data="/data/jenkins"
 
 mkdir -p "${host_data}/jenkins_home/init.groovy.d"
 
 pushd "${host_data}/jenkins_home/init.groovy.d/" || exit 1
-    curl -O -f#SL --retry 1 --connect-timeout 1 -m 10 "https://github.com/dyrnq/dist/raw/main/jenkins/init-jenkins.groovy" || \
+    curl ${curl_opts} -O -f#SL --retry 1 --connect-timeout 5 -m 20 "https://github.com/dyrnq/dist/raw/main/jenkins/init-jenkins.groovy" || \
     curl -O -f#SL --retry 10 "https://fastly.jsdelivr.net/gh/dyrnq/dist@main/jenkins/init-jenkins.groovy" || \
     curl -O -f#SL --retry 10 "https://testingcf.jsdelivr.net/gh/dyrnq/dist@main/jenkins/init-jenkins.groovy" || \
     exit 1
 popd || exit 1
 pushd "${host_data}" || exit 1
-    curl -O -f#SL --retry 1 --connect-timeout 1 -m 10 "https://github.com/dyrnq/dist/raw/main/jenkins/jvm.options" || \
+    curl ${curl_opts} -O -f#SL --retry 1 --connect-timeout 5 -m 20 "https://github.com/dyrnq/dist/raw/main/jenkins/jvm.options" || \
     curl -O -f#SL --retry 10 "https://fastly.jsdelivr.net/gh/dyrnq/dist@main/jenkins/jvm.options" || \
     curl -O -f#SL --retry 10 "https://testingcf.jsdelivr.net/gh/dyrnq/dist@main/jenkins/jvm.options" || \
     exit 1
